8d7b02129feadabca9c586a3e782119e1dd92ae3
Zubair <zubairabdulmatin@gmail.com>
2026-07-27 18:05:10 +0100

    ADDED: main file to test the program

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
-//
-// Created by zubair Abdul matin on 27/07/2026.
-//
+#include <iostream>
+#include "Vector2.hpp"
+
+using namespace physics;
+using namespace std;
+
+int main() {
+    Vector2 position{0.0f, 10.0f};
+    Vector2 velocity{5.0f, -2.0f};
+
+    position += velocity;
+
+    cout << "Vector Math Ready!\n";
+    cout << "New Position: (" << position.x << ", " << position.y << ")\n";
+
+    return 0;
+}
