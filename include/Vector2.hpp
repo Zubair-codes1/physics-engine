ef882e1c023933a4b423816701361d7d87234e3a
Zubair <zubairabdulmatin@gmail.com>
2026-07-27 18:22:00 +0100

    FIX: variable naming for x and y positions to make them more general

--- include/Vector2.hpp
+++ include/Vector2.hpp
@@ -4,50 +4,50 @@
 
 namespace physics {
     struct Vector2{
-        float x_pos{0.0f};
-        float y_pos{0.0f};
+        float x{0.0f};
+        float y{0.0f};
 
         constexpr Vector2() = default; // set to default values
-        constexpr Vector2(float x, float y) : x_pos(x), y_pos(y) {} // new vals
+        constexpr Vector2(float x, float y) : x(x), y(y) {} // new vals
 
         // operators
         Vector2 operator+(const Vector2 &rhs) const {
-            return {x_pos + rhs.x_pos, y_pos + rhs.y_pos}; 
+            return {x + rhs.x, y + rhs.y}; 
         }
         Vector2 operator-(const Vector2 &rhs) const {
-            return {x_pos - rhs.x_pos, y_pos - rhs.y_pos};
+            return {x - rhs.x, y - rhs.y};
         }
         Vector2 operator*(float scalarMultiply) const {
-            return {x_pos * scalarMultiply, y_pos * scalarMultiply};
+            return {x * scalarMultiply, y * scalarMultiply};
         }
         Vector2 operator/(float scalarDivide) const {
-            return {x_pos / scalarDivide, y_pos / scalarDivide};
+            return {x / scalarDivide, y / scalarDivide};
         }
         
 
         Vector2 operator+=(const Vector2 &rhs) { 
-            x_pos += rhs.x_pos;
-            y_pos += rhs.y_pos;
+            x += rhs.x;
+            y += rhs.y;
             return *this;
         }
 
         Vector2 operator-=(const Vector2 &rhs) {
-            x_pos -= rhs.x_pos;
-            y_pos -= rhs.y_pos;
+            x -= rhs.x;
+            x -= rhs.y;
             return *this;
         }
 
         Vector2 operator*=(float scalarMultiply) {
-            x_pos *= scalarMultiply;
-            y_pos *= scalarMultiply; 
+            x *= scalarMultiply;
+            y *= scalarMultiply; 
             return *this; 
         }
 
-        Vector2 operator-() {return {-x_pos, -y_pos};}
+        Vector2 operator-() {return {-x, -y};}
 
         // math functions
 
-        [[nodiscard]] float length_squared() const { return x_pos * x_pos + y_pos * y_pos; }
+        [[nodiscard]] float length_squared() const { return x * x + y * y; }
         [[nodiscard]] float length() const { return std::sqrt(length_squared());}
 
         [[nodiscard]] Vector2 normalised() const {
@@ -56,16 +56,15 @@ namespace physics {
         }
 
         [[nodiscard]] float dot_product(const Vector2 &rhs) const {
-            return x_pos * rhs.x_pos + y_pos * rhs.y_pos;
+            return x * rhs.x + y * rhs.y;
         }
 
         [[nodiscard]] float cross_product(const Vector2 &rhs) {
-            return x_pos * rhs.y_pos - y_pos * rhs.x_pos;
+            return x * rhs.y - y * rhs.x;
         }
     };
 
     inline Vector2 operator*(float scalar, Vector2 &vector) {
         return vector * scalar; 
-
     }
 }
